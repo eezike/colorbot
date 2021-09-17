72550b1e8f78dfc467d0f56d3192361c195771b2
Emeka Ezike <eezike@college.harvard.edu>
2021-09-17 18:15:02 -0400

    works w xpaths

--- color.py
+++ color.py
@@ -3,17 +3,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-import pickle
 import os
-import sys
 
-c = input("C: ")
-d = input("D: ")
 
 
 options = Options()
-options.headless = True
-driver = webdriver.Chrome(options=options)
+
+
+c = 0 #input("C: ")
+d = 0 #input("D: ")
+
+
+options.headless = False
+path = f"{os.getcwd()}/driver/chromedriver"
+
+driver = webdriver.Chrome(executable_path = path, options=options)
+
 
 # go
 driver.get('https://home.color.com/sign-in')
@@ -21,44 +26,36 @@ sleep(2)
 
 driver.find_element_by_name("email").send_keys("eezike@college.harvard.edu")
 driver.find_element_by_name("password").send_keys("")
-driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[3]/div/div/div/form/button').click()
+driver.find_element_by_xpath('//*[@id="root"]/div/div/div[3]/div/div/div/form/div[3]/button').click()
 sleep(2)
 
 driver.get("https://home.color.com/covid/activation/ready-to-activate")
 sleep(2)
 
 xpaths = [
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/form/button',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div/div/div/a',
-    '//*[@id="root"]/div[2]/div[3]/div/a',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/button',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/button',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[1]/div[4]/button',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/button',
-
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/div[4]/label/span[1]/span/input',
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/div[5]/label/span[1]/span/input',
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/div[6]/label/span[1]/span/input',
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/div[7]/label/span[1]/span/input',
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/div[8]/div[1]/a',
-
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/button',
-    '//*[@id="root"]/div[2]/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/button'
-
-    '//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/form/button',
-    '/html/body/div[3]/div[3]/div/div[3]/button[1]',
+    '//*[@id="onetrust-close-btn-container"]/button',
+    '//*[@id="root"]/div/div/div[3]/div/div/div[2]/div/div/div/a',
+    '//*[@id="root"]/div/div/div[3]/div/a',
+    '//*[@id="root"]/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/button',
+    '//*[@id="root"]/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/div/button',
+    '//*[@id="root"]/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[1]/div[4]/button',
+    '//*[@id="root"]/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/button',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/div[4]/label/span[1]/span[1]/input',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/div[5]/label/span[1]/span[1]/input',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/div[6]/label/span[1]/span[1]/input',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/div[7]/label/span[1]/span[1]/input',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/a',
+    '//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/form/div[9]/button',
+    '/html/body/div[3]/div[3]/div/div[3]/button[2]',
 ]
 
 for xpath in xpaths:
-    try:
-        driver.find_element_by_xpath(xpath).click()
-        sleep(3)
-    except:
-        pass
+    driver.find_element_by_xpath(xpath).click()
+    sleep(3)
 
 driver.find_element_by_name("kit_barcode").send_keys(d)
 driver.find_element_by_name("accession_number").send_keys(c)
-driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[3]/div[2]/div/div[1]/form/button').click()
+driver.find_element_by_xpath('//*[@id="root"]/div/div/div[3]/div[2]/div/div[1]/form/div[3]/button').click()
 sleep(2)
 
 driver.find_element_by_xpath('/html/body/div[3]/div[3]/div/div[3]/button[1]').click()
